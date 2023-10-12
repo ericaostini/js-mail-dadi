52b7c239e6af8cfd3da488599c1a6e1220c6f0f6
ericaostini <ericaostini14@gmail.com>
2023-10-12 16:10:09 +0200

    generate random number and display content div "gioco"

--- js/dadi.js
+++ js/dadi.js
@@ -5,3 +5,15 @@ function getRndInteger(min, max){
 
 // dadi
 const wrapperDadi = document.getElementById("dadi");
+const gioco = document.createElement("div");
+gioco.className= `container pt-3`;
+gioco.innerHTML = `
+    <h1 class = "text-primary">Gioco dei dadi</h1>
+    <p> Il programma genera un numero causale per l'utente e un numero casuale per il computer, chi ha il numero più alto vince! </p>
+`;
+wrapperDadi.append(gioco);
+let numberUser = getRndInteger(1,6);
+
+console.log(numberUser);
+let numberComputer = getRndInteger(1,6);
+console.log(numberComputer);
