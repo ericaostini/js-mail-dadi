bf74da2ad7ae4e1f7a5c856319dc3759fa4ea198
ericaostini <ericaostini14@gmail.com>
2023-10-12 17:03:09 +0200

    fixed alert message, email javascript page

--- js/script.js
+++ js/script.js
@@ -4,15 +4,10 @@ const container = document.createElement("div");
 container.className = `p-3`;
 container.innerHTML = `
     <h1> Inserisci la tua email </h1>
-    <form class="row">
-        <div class="col-6">
-            <label for = "email" class="visually-hidden"></label>
-            <input type="email" class="form-control" id="email" placeholder="Email">
-        </div>
-        <div class="col-2">
-            <button type="submit" class="btn btn-primary">Invia Email</button>
-        </div>
-    </form>
+    <label for="email"></label>
+    <input type="email""type=" class="form-control" placeholder = "email" id="email" name="email" >
+    <button class="btn btn-primary">Invia Email</button>
+    <div class="d-none alert">ciao</div>
 `;
 console.log(container);
 wrapper.append(container);
@@ -25,21 +20,27 @@ const listEmail = [
 ]
 console.log(listEmail);
 const button = document.querySelector("button");
-const alert = document.getElementById("alert");
+const displayAlert = document.querySelector(".alert");
 button.addEventListener("click", function(){
     let insertEmail = document.getElementById("email").value;
+    let found = false;
     for (e = 0; e < listEmail.length; e++){
         console.log(listEmail[e]);
         let checkEmail = listEmail[e];
-        let text, colorText, alertDiv;
         if (insertEmail === checkEmail){
-            text = "Email corretta";
-            alertDiv = "alert-success alert";
-        } else {
-            text = "Email non trovata"
-            alertDiv = "alert-danger alert";
+            found = true;
         }
-        container.innerHTML = text;
-        container.className = `${colorText} ${alertDiv}`;
-    }   
+    }
+    let text, buttonAlert;
+    if(found){
+        console.log("presente");
+        text = "Email presente";
+        buttonAlert = "alert-info";
+    } else {
+        text = "Email non presente";
+        buttonAlert = "alert-info";
+    }
+    displayAlert.innerHTML = text; 
+    displayAlert.classList.remove("d-none");
+    displayAlert.classList.add(buttonAlert); 
 })
